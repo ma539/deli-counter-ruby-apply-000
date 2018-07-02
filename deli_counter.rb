fca4fc8bfbd152cc275ca7dcc5a1e0cc05578eca
ma539@cornell.edu <ma539@cornell.edu>
2018-07-02 18:47:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,24 @@
 
 katz_deli = []
 
+
+
+def take_a_number(katz_deli)
+  count = 1
+  katz_deli << count
+  puts "Welcome! You're ticket number #{count}"
+  count +=1
+end
+
+# take_a_number(katz_deli) => "Welcome! You're ticket number #1" [1]
+# take_a_number(katz_deli) => "Welcome! You're ticket number #2" [1, 2]
+
+# now_serving(katz_deli) => "Currently Serving: #1" [2]
+# now_serving(katz_deli) => "Currently Serving: #2" []
+
+# take_a_number(katz_deli) => "Welcome! You're ticket number #3" [3]
+
+
 def take_a_number (katz_deli, name)
   katz_deli << name
   puts ("Welcome, #{name}. You are number #{katz_deli.length} in line.")
